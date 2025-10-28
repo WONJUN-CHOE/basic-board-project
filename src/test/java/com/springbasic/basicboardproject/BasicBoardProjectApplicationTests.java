218f9116f0475513ccf6f4b9f3877418577d668c
WONJUN-CHOE <chowj379@gmail.com>
2025-10-28 18:37:59 +0900

    #6 - 스프링 부트 프로젝트 시작
    * 스프링 웹
    * 스프링 엑츄에이터
    * 데브 툴즈
    * 롬복

---
+++ src/test/java/com/springbasic/basicboardproject/BasicBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbasic.basicboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
